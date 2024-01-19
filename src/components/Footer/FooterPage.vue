6c3db2c2fd9afd6106160e6209aa8aa881a3ce5d
vas1levnv <vas1levnv@yandex.ru>
2024-01-19 18:51:16 +0300

    Верстает футер

--- src/components/Footer/FooterPage.vue
+++ src/components/Footer/FooterPage.vue
@@ -2,7 +2,20 @@
 	<footer class="footer" :style="{'background-image': 'url(' + footerBg + ')'}">
 		<div class="container">
 			<div class="footer-wrapper">
-
+				<a href="#" class="footer-logo">
+					<fnl-icon/>
+				</a>
+				<div class="footer-list">
+					<a href="#">
+						<telegram-icon/>
+					</a>
+					<a href="#">
+						<vk-icon/>
+					</a>
+					<a href="#">
+						<youtube-icon/>
+					</a>
+				</div>
 			</div>
 		</div>
 	</footer>
@@ -10,9 +23,14 @@
 
 <script>
 import footerBg from '@/assets/img/footer-bg.png'
+import FnlIcon from "@/components/icons/FnlIcon.vue";
+import TelegramIcon from "@/components/icons/TelegramIcon.vue";
+import VkIcon from "@/components/icons/VkIcon.vue";
+import YoutubeIcon from "@/components/icons/YoutubeIcon.vue";
 
 export default {
 	name: "FooterPage",
+	components: {YoutubeIcon, VkIcon, TelegramIcon, FnlIcon},
 	data() {
 		return {
 			footerBg: footerBg,
@@ -23,10 +41,42 @@ export default {
 
 <style scoped lang="scss">
 .footer {
-	height: 40vh;
-	min-height: 380px;
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: top;
+	
+	&-wrapper {
+		display: flex;
+		justify-content: space-between;
+		align-items: end;
+		height: 40vh;
+		min-height: 380px;
+		padding: 5rem 0;
+	}
+	
+	&-logo{
+		width: 56px;
+		svg{
+			width: 100%;
+			height: 100%;
+		}
+	}
+	
+	&-list {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+}
+
+
+@media (max-width: 768px) {
+	.footer{
+		&-wrapper {
+			height: 20vh;
+			min-height: 200px;
+			padding: 2rem 0;
+		}
+	}
 }
 </style>
