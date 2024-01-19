00556ce95d3c6f6c4184418ec78973e7e016d0db
vas1levnv <vas1levnv@yandex.ru>
2024-01-19 12:55:44 +0300

    Верстает описания команд

--- src/components/TeamsDescription/TeamsDescription.vue
+++ src/components/TeamsDescription/TeamsDescription.vue
@@ -7,11 +7,16 @@
 						<div class="descr-item__img"><img :src="item.src" alt="item.name"></div>
 						<div class="descr-item__content">
 							<div class="descr-item__name">{{ item.name }}</div>
-							<div class="descr-item__year">Год основания - {{ item.year }}</div>
-							<div class="descr-item__championship" v-if="item.championship">Чемпионат - {{ item?.championship }}</div>
+							<div class="descr-item__year"><span class="orange-text">Год основания</span> - {{
+									item.year
+								}}
+							</div>
+							<div class="descr-item__championship" v-if="item.championship"><span class="orange-text">Чемпионат</span>
+								- {{ item?.championship }}
+							</div>
 							<div>{{ item.description }}</div>
 							<div class="descr-item__achievements">
-								<div>Достижения</div>
+								<div class="orange-text">Достижения</div>
 								<ul>
 									<li v-for="el in item.achievements" :key="el.id">
 										{{ el.text }}
@@ -19,10 +24,12 @@
 								</ul>
 							</div>
 							<div class="descr-item__persons">
-								<div>Главные лица</div>
-								<div v-for="person in item.persons" :key="person.id">
-									<img :src="person.src" :alt="person.name">
-									<div>{{ person.name }}</div>
+								<div class="orange-text">Главные лица</div>
+								<div class="descr-item__persons-list">
+									<div class="descr-item__persons-item" v-for="person in item.persons" :key="person.id">
+										<img :src="person.src" :alt="person.name">
+										<div class="descr-item__persons-name">{{ person.name }}</div>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -264,14 +271,53 @@ export default {
 	&-item {
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
 		margin-bottom: 6.5rem;
+		font-weight: 500;
+		font-size: 1.5rem;
+		gap: 5rem;
+		
+		&__img {
+			img {
+				width: 100%;
+			}
+		}
 		
 		&:nth-child(2n) {
 			flex-direction: row-reverse;
 		}
 		
-		& .descr-item__content {
+		&__content {
 			width: 50%;
+			display: flex;
+			flex-direction: column;
+			gap: 1.25rem;
+		}
+		
+		&__name {
+			font-size: 2.25rem;
+		}
+		
+		&__achievements {
+			ul {
+				margin-top: 1.25rem;
+				padding-left: 1.25rem;
+			}
+		}
+		
+		&__persons {
+			&-list {
+				display: flex;
+				align-items: center;
+				gap: 1.25rem;
+				margin-top: 1.25rem;
+			}
+			
+			&-name {
+				margin: 0.5rem auto 0;
+				width: min-content;
+				text-align: center;
+			}
 		}
 	}
 }
