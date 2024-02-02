a5ee4065e857270a6517521d4e22b541da1537cc
vas1levnv <vas1levnv@yandex.ru>
2024-02-02 15:23:56 +0300

    Пишет скрипт для скрола

---
+++ prod/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+	
+	// секции
+	const btnsList = document.querySelectorAll('.teams-links-item')
+	const anchorsList = document.querySelectorAll('[data-anchors]')
+	btnsList.forEach(el => el.addEventListener('click', function() {
+		anchorsList.forEach(item => {
+			if(this.dataset.src === item.dataset.anchors) {
+				item.scrollIntoView({behavior: "smooth"})
+			}
+		})
+	}))
+	
+	// команды
+	
+	const btnsTeamsList = document.querySelectorAll('[data-teams]')
+	const teamsList = document.querySelectorAll('[data-detail]')
+	btnsTeamsList.forEach(el => el.addEventListener('click', function() {
+		teamsList.forEach(item => {
+			if(this.dataset.teams === item.dataset.detail) {
+				item.scrollIntoView({behavior: "smooth"})
+			}
+		})
+	}))
+})
